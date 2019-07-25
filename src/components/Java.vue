d2556adba07f69693b4f71dadc1985eecf7fb30a
Shlomo <shlomo809@gmail.com>
2019-07-26 02:07:52 +0300

    java guid

---
+++ src/components/Java.vue
@@ -0,0 +1,79 @@
+<template>
+  <div class="bobi">
+    <br />
+    <img src="../assets/KKK.png" class="BOB" />
+    <br />
+    <p>
+      <u>
+        <font size="100">Walcome to Java tutorial</font>
+      </u>
+    </p>
+    <br />
+    <br />
+    <font size="5">
+      Hello here we going to learn the basic of Java.
+      first of all you need to know what is Java.
+      Java is a programming language and computing platform. There are lots of applications and websites
+      that will not work unless you have Java installed, and more are created every day.
+      Java is fast, secure, and reliable.
+      From laptops to datacenters, game consoles to
+      scientific supercomputers, cell phones to the Internet, Java is everywhere!</font>
+    
+    <br />
+
+    <br />
+    <p>
+      <u>
+        <font size="100">Every start of Java.</font>
+      </u>
+    </p>
+    <br />
+    <br />
+    <font size="5">
+      in every start of Java pgroram you need to start with the command "public static main"
+      and then you need to write : "public static void main(String[] args)" after this you need to wrirht this sign "{" this sign is start of the code every thing you
+       put het until ""}"" the computer will go over code and do every command.
+
+    </font>
+    <br />
+    <br />
+    <p>
+      <u>
+        <font size="100">How to print in Java.</font>
+      </u>
+    </p>
+    <br /><font size="5">After we did the first two basic lines we want to write somthing and that the computer will print it.
+       the command of printing in Java is: "system.out.println("");".
+       lets say we want the computer to print us "Hello World", so what thet we need to do is writr system.out.println("Hello World");
+       and then the in the bottom of the computer will see Hello World.  </font>
+    <br />
+    her is a video thet
+    <a href="https://www.youtube.com/watch?v=KDuse0mmTm4">
+     
+      <br />
+    </a>
+
+ 
+
+  </div>
+</template>
+<style>
+.BOB {
+  width: 900px;
+  height: 300px;
+}
+</style>
+<style>
+.bobi {
+  width: 1000px;
+  margin: 0 auto;
+}
+</style>
+<script>
+// @ is an alias to /src
+
+export default {
+  name: "JavascriptComponent",
+  components: {}
+};
+</script>

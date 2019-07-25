2761152c655703172cbbb9d5aa86bf4319e5c833
Yoel Siegel <yoelsiegel@gmail.com>
2019-07-25 23:20:54 +0300

    start of vue project

---
+++ src/components/Homepage.vue
@@ -0,0 +1,37 @@
+<template>
+  <div class="hello">
+    <h1>{{ msg1 }}</h1>
+   <p>
+   <h2>{{ msg2 }}</h2>
+  </div>
+
+  <img src="htmlpic.jpeg" alt="Italian Trulli">
+</template>
+
+<script>
+export default {
+  name: 'Homepage',
+  props: {
+    msg1: String,
+    msg2: String, 
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h3 {
+  margin: 40px 0 0;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+}
+</style>
---
+++ src/views/Home.vue
@@ -0,0 +1,21 @@
+<template>
+  <div class="home">
+    <img alt="Vue logo" src="../assets/hackathon.jpeg">
+    <Homepage msg1="Welcome to Hackalearn"/>
+    <Homepage msg2= "Learn today, code tomorrow"/>
+    <img src="htmlpic.jpeg" alt="Italian Trulli">
+  </div>
+
+</template>
+
+<script>
+// @ is an alias to /src
+import Homepage from '@/components/Homepage.vue'
+
+export default {
+  name: 'home',
+  components: {
+    Homepage
+  }
+}
+</script>

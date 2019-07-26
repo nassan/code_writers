adb2bef6c79b9970db2b563a6572e3ab72763b91
Shlomo <shlomo809@gmail.com>
2019-07-26 05:01:34 +0300

    java guid

--- src/components/Java.vue
+++ src/components/Java.vue
@@ -5,14 +5,14 @@
     <br />
     <p>
       <u>
-        <font size="100">Walcome to Java tutorial</font>
+        <font size="100">Welcome to Java tutorial</font>
       </u>
     </p>
     <br />
     <br />
     <font size="5">
-      Hello here we going to learn the basic of Java.
-      first of all you need to know what is Java.
+      Hello here we going to learn the basics of Java.
+      First of all, you need to know what Java is.
       Java is a programming language and computing platform. There are lots of applications and websites
       that will not work unless you have Java installed, and more are created every day.
       Java is fast, secure, and reliable.
@@ -30,47 +30,111 @@
     <br />
     <br />
     <font size="5">
-      in every start of Java pgroram you need to start with the command "public static main"
-      and then you need to write : "public static void main(String[] args)" after this you need to wrirht this sign "{" this sign is start of the code every thing you
-       put het until ""}"" the computer will go over code and do every command.
+      In every start of Java pgroram you need to start with the command "public static main"
+      and then you need to write : "public static void main(String[] args)" after this you need to write this sign "{" This sign is start of the code everything you
+       put here until ""}"" the computer will go over code and do every command.
 
     </font>
     <br />
     <br />
     <p>
-      <u>
+      
         <font size="100">How to print in Java.</font>
-      </u>
+      
     </p>
     <br /><font size="5">After we did the first two basic lines we want to write somthing and that the computer will print it.
        the command of printing in Java is: "system.out.println("");".
-       lets say we want the computer to print us "Hello World", so what thet we need to do is writr system.out.println("Hello World");
-       and then the in the bottom of the computer will see Hello World.  </font>
-    <br />              
-    <font size="5">Her is a video thet sum it up:
-      
+       lets say we want the computer to print us "Hello World", so what thet we need to do is write system.out.println("Hello World");
+       and then the in the bottom of the computer will see Hello World. </font>
+        <br /> 
+         <p>
+      <u> 
+        <font size="100"> Variables</font>
+           </u>
+    </p>
+         <br />  
+        <font size="5">
+         Ther are number of diffrent variables in Java for example "int" int is for all the Entire variables.
+         lets say you want to define thet x=5 so to do that you need to set the x in the int.
+         for exampl int x = 5; , and then when you print x the computer will show 5.
+         if we want to define latters we need to define them by "char" and words by "string" and גecimal numbers by "float"</font>
+
+         
+
+    <br />  
+    <br> 
+    <br>           
+    <font size="6">Here is a video thet sum it up:
+
     </font>
-    <a href="https://www.youtube.com/watch?v=KDuse0mmTm4">
-     clic me!
+    <font size="6"> <a href="https://www.youtube.com/watch?v=KDuse0mmTm4" target="_blank">    
+     click me!
       <br />
-    </a>
+      <br>
+      <br>
+      <br>
+      <br>
+     
+    </a></font>
+     <font size="6">here is some more links:</font>
+    <br>
+    <br>
+  <br>
+  <font size="6"><a href="https://www.youtube.com/watch?v=S2C9s7kb2BQ" target="_blank">
+  click 1!
+  <br>
+</a></font>
+<font size="6"><a href="https://www.youtube.com/watch?v=zEdKgzmm-2g">
+click 2!
 
- 
+</a></font>
+<br>
+<br>
+<br>
+<br>
 
+<font size="8"> good luck!</font>
+ <br>
+ <br>
+ <br>
+ <br>
+ <br>
+ <br>
+ <br>
+ <br>
+ <br>
+ <br>
+ <br>
+
+<div>
+
+</div>
   </div>
+ 
 </template>
 <style>
 .BOB {
   width: 900px;
   height: 300px;
+  background: url("../assets/KKK.png")
+
 }
-</style>
-<style>
 .bobi {
   width: 1000px;
   margin: 0 auto;
 }
+body{
+  background-image: url("../assets/ddd.jpeg") ; background-size: 100% 100%; 
+  opacity: 2.5;
+}
+
+
+
 </style>
+
+
+
+ 
 <script>
 // @ is an alias to /src
 
